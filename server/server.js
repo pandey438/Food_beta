a59cd6cd3e39c7c515f2f3adb01a7c546bdcb99b
Parmarth <130452891+pandey438@users.noreply.github.com>
2024-06-20 19:15:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,12 +19,6 @@ app.use("/", (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
